538b7933c41ae3c04b425f36bef2d75d7d33abaf
Adan Abbi <adanweli@hotmail.com>
2021-04-27 23:05:09 -0700

    js code about name

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ console.log('adding JS to our project')
 
 const name = 'ali'; 
 
+ function getString(name){
+   return <h1>` hi my name is ${name}`</h1>
+}
+
+console.log(getString('Mark Philips'))
